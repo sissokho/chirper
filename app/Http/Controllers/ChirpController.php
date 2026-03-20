9ed63b1e4ba4ef7843874043d05de12726a73ce1
Mouhamadou Moustapha Sissokho <siskomouhamed@gmail.com>
2026-03-20 12:10:22 +0000

    Display actual chirps from the db

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Chirp;
 use Illuminate\Http\Request;
 
 class ChirpController extends Controller
@@ -11,7 +12,12 @@ class ChirpController extends Controller
      */
     public function index()
     {
-        //
+        $chirps = Chirp::with('user')
+            ->latest()
+            ->take(50)
+            ->get();
+
+        return view('home', ['chirps' => $chirps]);
     }
 
     /**
