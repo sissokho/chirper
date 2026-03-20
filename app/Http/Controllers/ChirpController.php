3d767f659a02b265a9a49c210d8f9a3e8294ebc4
Mouhamadou Moustapha Sissokho <siskomouhamed@gmail.com>
2026-03-20 18:44:50 +0000

    Create chirps and show success notification

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -33,7 +33,19 @@ class ChirpController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validated = $request->validate([
+            'message' => ['required', 'string', 'max:255'],
+        ], [
+            'message.required' => 'Please write something to chirp!',
+            'message.max' => 'Chirps must be 255 characters or less.',
+        ]);
+
+        Chirp::create([
+            'message' => $validated['message'],
+            'user_id' => null,
+        ]);
+
+        return redirect('/')->with('success', 'Your chirp has been posted!');
     }
 
     /**
